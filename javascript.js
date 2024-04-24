03eb26a7026757d877863e9ba0a7be8a90a06732
Kocannn <dwicandraandika4@gmail.com>
2024-04-24 15:45:54 +0800

    menambahkan logo

--- javascript.js
+++ javascript.js
@@ -1,7 +1,8 @@
 const nav = document.querySelector('nav');
 const tombolMenu = document.querySelector('.button_menu');
 const menu = document.querySelector('nav .menu ul');
-
+const logoHitam = document.querySelector('nav .logo img.hitam');
+const logoPutih = document.querySelector('nav .logo img.putih');
 function klikMenu(){
   tombolMenu.addEventListener('click', () => {
     if(menu.style.display === 'none') {
@@ -28,9 +29,15 @@ document.addEventListener('DOMContentLoaded', () => {
     scroll_pos = window.scrollY;
     if(scroll_pos > 0){
       nav.classList.add('putih');
+      logoHitam.style.display ='block';
+      logoPutih.style.display ='none';
     }else{
       nav.classList.remove('putih');
+      logoHitam.style.display ='none';
+      logoPutih.style.display ='block';
     }
+    klikMenu();
+    
   })
 })
 
