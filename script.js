d54c40ac7e2dba0110c20b4da003720539bc1615
Kocannn <dwicandraandika4@gmail.com>
2024-04-28 19:04:00 +0800

    bingung mau komen apa part 2

--- script.js
+++ script.js
@@ -2,7 +2,12 @@ const url = "/data/wisata.json";
 const judul = document.getElementById("judul");
 const isi = document.getElementById('content-text');
 const lokasi = document.getElementById('lokasi');
-const gambar = document.getElementById('img')
+const gambar = document.getElementById('img');
+const iklan = document.getElementById('iklan');
+
+function getRandomValue(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 async function getData(){
   const response = await fetch(url);
@@ -11,13 +16,58 @@ async function getData(){
   }
   const data = await response.json();
   const {wisata} = data;
-
-  for (const index in wisata){
-    if(judul.textContent.toLowerCase().includes(wisata[index].nama.toLowerCase())){
-      isi.innerText = wisata[index].teks;
-      lokasi.innerText = wisata[index].lokasi;
-      gambar.innerHTML = `<img src="/Asset/${wisata[index].gambar}" alt="">`
+  const jumlah_data = wisata.length;
+  let randomValue1 = getRandomValue(0, jumlah_data - 1);
+  let randomValue2 = getRandomValue(0, jumlah_data - 1);
+  let indexWisata = findIndex(judul.textContent);
+  
+  for (const indexAtas in wisata){
+    if(judul.textContent.toLowerCase().includes(wisata[indexAtas].nama.toLowerCase())){
+      isi.innerText = wisata[indexAtas].teks;
+      lokasi.innerText = wisata[indexAtas].lokasi;
+      gambar.innerHTML = `<img src="/Asset/${wisata[indexAtas].gambar}" alt="">`
     }
   }
+  
+  function buatIklan(){
+    if(randomValue1 != randomValue2 && randomValue1 != indexWisata && randomValue2 != indexWisata){
+      iklan.innerHTML = `
+       <a href="/${wisata[randomValue1].link}">
+        <div class="gambar">
+           <img src="Asset/${wisata[randomValue1].gambar}" alt="">
+           <div class="text-iklan">    
+              <h4>${wisata[randomValue1].nama}</h4>
+              <p>${wisata[randomValue1].ringkasan}</p>
+           </div>
+        </div>
+       </a>
+       <a href="/${wisata[randomValue2].link}">
+         <div class="gambar">
+           <img src="Asset/${wisata[randomValue2].gambar}" alt="">
+           <div class="text-iklan">    
+              <h4>${wisata[randomValue2].nama}</h4>
+              <p>${wisata[randomValue2].ringkasan}</p>
+           </div>
+         </div>
+       </a>
+       `
+    } else {
+      randomValue1 = getRandomValue(0, wisata.length - 1);
+      randomValue2 = getRandomValue(0, wisata.length - 1);
+      buatIklan();
+    }
+  }
+  
+  buatIklan();
+
+  function findIndex(value){
+    for (const index in wisata){
+      if(wisata[index].nama === value){
+        return index;
+      }
+    }
+    return -1;
+  }
 }
-document.addEventListener('DOMContentLoaded', getData());
+
+window.addEventListener('DOMContentLoaded', getData());
