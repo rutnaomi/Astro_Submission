301b41daf8e875366d0f7a6466758b8bde6b98fe
Kocannn <dwicandraandika4@gmail.com>
2024-05-01 16:00:46 +0800

    membuat halaman destinasi

---
+++ destinasi.js
@@ -0,0 +1,103 @@
+const artikel = document.querySelector(".container-artikel");
+const url = "/data/wisata.json";
+const sidebar = document.querySelector('.container-kanan');
+const sidebar_content = document.querySelector('.content-wrapper');
+
+window.onscroll = () =>{
+  let scrollTop = window.scrollY;
+  let viewportHeight = window.innerHeight;
+  let contentHeight = sidebar_content.getBoundingClientRect().height;
+
+  if(scrollTop >= viewportHeight - contentHeight){
+    sidebar_content.classList.add('is-fixed');
+  }else{
+    sidebar_content.classList.remove('is-fixed');
+  }
+}
+
+
+
+function getRandomValue(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+
+
+
+
+async function getData(){
+  const response = await fetch(url);
+  const data = await response.json();
+
+  for (const index in data.wisata){
+    artikel.innerHTML += `
+           <div class="container"> 
+              <div id="img">
+                <img src="/Asset/${data.wisata[index].gambar}" alt="">
+              </div>
+              <a href="/${data.wisata[index].link}">
+                <h2 id="judul">${data.wisata[index].nama}</h2>
+              </a>
+              <div class="content-text">
+                <p id="content-text">
+                  ${data.wisata[index].deskripsi}
+                </p>
+                <div class="container-location">
+                  <img src="/Asset/location.png" alt="">
+                  <p id="lokasi">
+                    ${data.wisata[index].lokasi}
+                  </p>
+                </div>
+              </div>  
+              <hr>
+            </div>
+    `
+  }
+  
+  function buatIklan(){
+    const {wisata} = data; 
+    const jumlah_data = wisata.length;
+    let randomValue1 = getRandomValue(0, jumlah_data - 1);
+    let randomValue2 = getRandomValue(0, jumlah_data - 1);
+    let indexWisata = findIndex(judul.textContent);
+    if(randomValue1 != randomValue2 && randomValue1 != indexWisata && randomValue2 != indexWisata){
+      iklan.innerHTML = `
+       <a href="/${wisata[randomValue1].link}">
+        <div class="gambar">
+           <img src="Asset/${wisata[randomValue1].gambar}" alt="">
+           <div class="text-iklan">    
+              <h4>${wisata[randomValue1].nama}</h4>
+              <p>${wisata[randomValue1].ringkasan}</p>
+           </div>
+        </div>
+       </a>
+       <a href="/${wisata[randomValue2].link}">
+         <div class="gambar">
+           <img src="Asset/${wisata[randomValue2].gambar}" alt="">
+           <div class="text-iklan">    
+              <h4>${wisata[randomValue2].nama}</h4>
+              <p>${wisata[randomValue2].ringkasan}</p>
+           </div>
+         </div>
+       </a>
+       `
+    } else {
+      randomValue1 = getRandomValue(0, wisata.length - 1);
+      randomValue2 = getRandomValue(0, wisata.length - 1);
+      buatIklan();
+    }
+    function findIndex(value){
+      for (const index in wisata){
+        if(wisata[index].nama === value){
+          return index;
+        }
+      }
+      return -1;
+    }
+  }
+  buatIklan();
+}
+document.addEventListener('DOMContentLoaded',() => {
+  getData();
+});
